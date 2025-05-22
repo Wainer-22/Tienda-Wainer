d27c23e61d8ddb87447e7ce38e861885c2edbe92
Wainer Oreamuno <owainer90398@ufide.ac.cr>
2025-05-21 19:14:28 -0600

    Commit 1
    Mi primer commit

---
+++ Tienda_Wainer/src/main/java/Tienda/Web/TiendaWainerApplication.java
@@ -0,0 +1,13 @@
+package Tienda.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaWainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaWainerApplication.class, args);
+	}
+
+}

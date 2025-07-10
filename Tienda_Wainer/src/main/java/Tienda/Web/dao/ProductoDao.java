e205991b334edc5c7219027f57b2d6da2c29ea36
Wainer22 <wainercr22@gmail.com>
2025-07-09 21:01:07 -0600

    Trabajo en clase - Semana 9

--- Tienda_Wainer/src/main/java/Tienda/Web/dao/ProductoDao.java
+++ Tienda_Wainer/src/main/java/Tienda/Web/dao/ProductoDao.java
@@ -1,9 +1,10 @@
 package Tienda.Web.dao;
 
 import Tienda.Web.domain.Producto;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductoDao extends JpaRepository <Producto, Long> {
-    
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
 }
  

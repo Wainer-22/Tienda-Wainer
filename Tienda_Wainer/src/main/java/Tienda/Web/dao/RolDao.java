f8b1f9530ab1e37438e072db48ae440e82cd3707
Wainer22 <wainercr22@gmail.com>
2025-07-16 20:44:36 -0600

    Semana 10 - Trabajo en clase

---
+++ Tienda_Wainer/src/main/java/Tienda/Web/dao/RolDao.java
@@ -0,0 +1,9 @@
+package Tienda.Web.dao;
+
+import Tienda.Web.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
+ 
---
+++ Tienda_Wainer/src/main/java/Tienda/Web/service/UsuarioDetailsService.java
@@ -0,0 +1,11 @@
+package Tienda.Web.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UsuarioDetailsService {
+    public UserDetails 
+        loadUserByUsername(String username)
+                throws UsernameNotFoundException;
+}
+ 
